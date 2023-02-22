096fc8e8bc0d34f92a0c8b2246810132b3b795b7
JoeLoera <joeloera7@gmail.com>
2023-02-22 08:01:04 -0800

    Confetti Added

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 const sliderFill = document.querySelector(".fill");
 const timeLeftText = document.querySelector("#time-left");
-const startCount = 5;
+const startCount = 60;
 let timeLeft = startCount;
 
 const timerId = setInterval(() => {
@@ -8,28 +8,31 @@ const timerId = setInterval(() => {
   timeLeft--;
   timeLeftText.textContent = timeLeft;
   sliderFill.style.width = (timeLeft / startCount) * 100 + "%";
-  slider.value = timeLeft;
-  console.log(slider.value, "after");
   if (timeLeft <= 0) {
     clearInterval(timerId);
     timeLeftText.textContent = "FIN!";
+    surprise();
   }
 }, 1000);
 
 function surprise() {
   const colors = [
-    "rgba(255,105,97, 0.5)",
+    "rgba(255, 105, 97, 0.5)",
     "rgba(97, 168, 255, 0.5)",
     "rgba(247, 255, 97, 0.5)",
-    "rgba(255,255,255, 0.5)",
+    "rgba(97, 255, 184, 0.5)",
+    "rgba(255, 255, 255, 0.5)",
   ];
 
   for (let i = 0; i < 500; i++) {
     setTimeout(() => {
-      console.log("boo");
       const circleElement = document.createElement("div");
       circleElement.classList.add("circle");
-      timeLeftText.append(circle);
+      circleElement.style.left = Math.floor(Math.random() * 100) + "%";
+      circleElement.style.top = Math.floor(Math.random() * 100) + "%";
+      circleElement.style.backgroundColor =
+        colors[Math.floor(Math.random() * colors.length)];
+      timeLeftText.append(circleElement);
     }, i * 5);
   }
 }
